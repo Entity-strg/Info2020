b9ca4cceb03dfd294d496b2b8607f143b824f7e8
YannickBougaran <YannickBougaran@0-208-13>
2020-12-08 15:08:55 +0100

    puplic String + return

---
+++ Spiel/src/TestVertex.java
@@ -0,0 +1,14 @@
+
+public class TestVertex {
+
+	public static void main(String[] args) {
+		
+		Vertex v1 = new Vertex(42,4);
+		Vertex v2 = new Vertex(0,0);
+		
+		System.out.println("v1.x = "+v1.x+", v1.y= "+v1.y);
+		
+		
+	}
+
+}
--- Spiel/src/Vertex.java
+++ Spiel/src/Vertex.java
@@ -3,12 +3,16 @@ public class Vertex {
 
 	public double x;
 	public double y;
-	
+
 	public Vertex(double x, double y) {
-		
-		
-	this.x=x;
-	this.y=y;
+
+		this.x = x;
+		this.y = y;
 	}
-	
+
+	puplic String toString() {
+		return "("+x+","+y+")";
+		
+	}
+
 }
